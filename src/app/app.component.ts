ab011abcd8ff0fcf4cb28867dd5044e7f5c74b1a
Marcos Gabriel <jorgede1097@gmail.com>
2021-12-19 11:24:02 -0300

    Adicionando arquivos mas sem compilação - error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {AppService} from './app.service';  
+import { FormGroup, FormControl,Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PagamentoCartao';
+  constructor(private AppService: AppService) { }  
+  data: any = FormGroup;  
+  CartaoForm!: FormGroup;  
+  submitted = false;   
+  EventValue: any = "Salvar";    
+  ngOnInit(): void {  
+    this.getdata();    
+    this.CartaoForm = new FormGroup({  
+      PagamentoId: new FormControl(null),  
+      NomeTitular: new FormControl("",[Validators.required]),        
+      NumeroCartao: new FormControl("",[Validators.required]),  
+      DataExpiracao:new FormControl("",[Validators.required]),  
+      CVV: new FormControl("",[Validators.required]),  
+    })    
+  }  
+
+  getdata() {  
+    this.AppService.getData().subscribe((data) => {  
+      this.data = data;  
+    })  
+  }  
+
+  deleteData(id: string) {  
+    this.AppService.deleteData(id).subscribe((data) => {  
+      this.data = data;  
+      this.getdata();  
+    })  
+  }  
+
+  Save() {   
+    this.submitted = true;      
+     if (this.CartaoForm.invalid) {  
+            return;  
+     }  
+    this.AppService.postData(this.CartaoForm.value).subscribe((data) => {  
+      this.data = data;  
+      this.resetFrom();   
+    })  
+  }  
+  Update() {   
+    this.submitted = true;      
+    if (this.CartaoForm.invalid) {  
+     return;  
+    }        
+    this.AppService.putData(this.CartaoForm.value.PagamentoId,
+             this.CartaoForm.value).subscribe((data) => {  
+      this.data = data;  
+      this.resetFrom();  
+    })  
+  }  
+  
+  EditData(Data: any) {  
+    this.CartaoForm.controls["PagamentoId"].setValue(Data.PagamentoId);  
+    this.CartaoForm.controls["NomeTitular"].setValue(Data.NomeTitular);      
+    this.CartaoForm.controls["NumeroCartao"].setValue(Data.NumeroCartao);  
+    this.CartaoForm.controls["DataExpiracao"].setValue(Data.DataExpiracao);  
+    this.CartaoForm.controls["CVV"].setValue(Data.CVV);  
+    this.EventValue = "Atualizar";  
+  }    
+  resetFrom()  
+  {     
+    this.getdata();  
+    this.CartaoForm.reset();  
+    this.EventValue = "Salvar";  
+    this.submitted = false;   
+  } 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
+import { HttpClientModule }    from '@angular/common/http';  
+import { AppService } from './app.service';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,  
+    ReactiveFormsModule,  
+    HttpClientModule 
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
